2f22c57c4e8d9d32c2de28bf776edd041ea675b5
Benoit Sagols <benoit.sagols@gmail.com>
2020-03-29 17:48:02 +0700

    Remove add command (single command)

--- index.js
+++ index.js
@@ -18,146 +18,144 @@ function getFilepath(file) {
   return filename.replace(/\\/g, "/");
 }
 
-const commands = {
-  async add(id, cmd) {
-    let url;
+async function importGist(id, cmd) {
+  let url;
 
-    // Get gist
-    url = `https://api.github.com/gists/${id}`;
+  // Get gist
+  url = `https://api.github.com/gists/${id}`;
 
-    log(`\nFetching gist...`.blue);
-    log(url);
+  log(`\nFetching gist...`.blue);
+  log(url);
 
-    let data;
+  let data;
 
-    try {
-      data = await request.get({
-        url,
-        headers: { "User-Agent": "yown" },
-        json: true
-      });
-    } catch (err) {
-      logError(`\nError fetching gist`.red);
-      log(err);
-      process.exit(1);
+  try {
+    data = await request.get({
+      url,
+      headers: { "User-Agent": "yown" },
+      json: true
+    });
+  } catch (err) {
+    logError(`\nError fetching gist`.red);
+    log(err);
+    process.exit(1);
+  }
+
+  // Download gist zip
+  url = `https://gist.github.com/${data.owner.login}/${id}/archive/${data.history[0].version}.zip`;
+
+  log(`\nDownloading gist archive...`.blue);
+  log(url);
+
+  try {
+    data = await request.get({ url, encoding: null });
+  } catch (err) {
+    logError(`\nError downloading gist archive`.red);
+    log(err);
+    process.exit(1);
+  }
+
+  log(`\nUnzipping gist archive...\n`.blue);
+
+  const archive = await zip.loadAsync(data);
+  const files = Object.values(archive.files);
+  const logs = [];
+
+  let copied = false;
+  let skipped = false;
+  let forced = false;
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const filepath = getFilepath(file);
+
+    // Filter
+    if (file.dir || filepath === "yown.json") {
+      continue;
     }
 
-    // Download gist zip
-    url = `https://gist.github.com/${data.owner.login}/${id}/archive/${data.history[0].version}.zip`;
+    // Exists ?
+    const exists = await fs.pathExists(filepath);
 
-    log(`\nDownloading gist archive...`.blue);
-    log(url);
-
-    try {
-      data = await request.get({ url, encoding: null });
-    } catch (err) {
-      logError(`\nError downloading gist archive`.red);
-      log(err);
-      process.exit(1);
-    }
-
-    log(`\nUnzipping gist archive...\n`.blue);
-
-    const archive = await zip.loadAsync(data);
-    const files = Object.values(archive.files);
-    const logs = [];
-
-    let copied = false;
-    let skipped = false;
-    let forced = false;
-
-    for (let i = 0; i < files.length; i++) {
-      const file = files[i];
-      const filepath = getFilepath(file);
-
-      // Filter
-      if (file.dir || filepath === "yown.json") {
-        continue;
-      }
-
-      // Exists ?
-      const exists = await fs.pathExists(filepath);
-
-      if (!cmd.force) {
-        if (exists) {
-          skipped = true;
-          logs.push("S ".white + filepath.grey);
-        } else {
-          copied = true;
-          logs.push("C ".green + filepath.grey);
-        }
-
-        continue;
-      }
-
-      // Overwrite ?
-      let overwrite = !exists || cmd.overwrite;
-
-      if (!overwrite) {
-        const { confirmed } = await prompt({
-          type: "confirm",
-          name: "confirmed",
-          message: `Replace ${filepath.grey}?`
-        });
-
-        overwrite = confirmed;
-      }
-
-      if (!overwrite) {
+    if (cmd.dryRun) {
+      if (exists) {
         skipped = true;
         logs.push("S ".white + filepath.grey);
-
-        continue;
-      }
-
-      // Copy
-      const buff = await file.async("nodebuffer");
-
-      await fs.outputFile(filepath, buff);
-
-      if (exists) {
-        forced = true;
-        logs.push("F ".red + filepath.grey);
       } else {
         copied = true;
         logs.push("C ".green + filepath.grey);
       }
+
+      continue;
     }
 
-    if (cmd.force) {
-      log(" ");
+    // Overwrite ?
+    let overwrite = !exists || cmd.force;
+
+    if (!overwrite) {
+      const { confirmed } = await prompt({
+        type: "confirm",
+        name: "confirmed",
+        message: `Replace ${filepath.grey}?`
+      });
+
+      overwrite = confirmed;
+    }
+
+    if (!overwrite) {
+      skipped = true;
+      logs.push("S ".white + filepath.grey);
+
+      continue;
+    }
+
+    // Copy
+    const buff = await file.async("nodebuffer");
+
+    await fs.outputFile(filepath, buff);
+
+    if (exists) {
+      forced = true;
+      logs.push("F ".red + filepath.grey);
     } else {
-      log("DRY RUN".green);
-    }
-
-    logs.forEach(txt => log(txt));
-
-    log(" ");
-
-    if (copied) {
-      log("C ".green + cmd.force ? "= Copied" : "= Copy");
-    }
-
-    if (forced) {
-      log("F ".red + "= Copied (Forced)");
-    }
-
-    if (skipped) {
-      log("S ".white + cmd.force ? "= Skipped" : "= Skip");
-    }
-
-    if (!cmd.force) {
-      log("\nUse --force to copy files");
+      copied = true;
+      logs.push("C ".green + filepath.grey);
     }
   }
-};
+
+  if (cmd.dryRun) {
+    log("DRY RUN".green);
+  } else {
+    log(" ");
+  }
+
+  logs.forEach(txt => log(txt));
+
+  log(" ");
+
+  if (copied) {
+    log("C ".green + (cmd.dryRun ? "= Copy" : "= Copied"));
+  }
+
+  if (forced) {
+    log("F ".red + "= Copied (Forced)");
+  }
+
+  if (skipped) {
+    log("S ".white + (cmd.dryRun ? "= Skip" : "= Skipped"));
+  }
+
+  if (cmd.dryRun) {
+    log("\nUse --force to copy files");
+  }
+}
 
 commander
   .version(info.version)
-  .command("add <id>")
-  .option("--force", "No dry run")
-  .option("--overwrite", "Force file overwrite")
-  .action(commands.add);
+  .option("-d, --dry-run", "Dry run (nothing copied)")
+  .option("-f, --force", "Force copy when file exists (overwrite)")
+  .arguments("<id>")
+  .action(importGist);
 
 if (!process.argv.slice(2).length) {
   commander.outputHelp();
