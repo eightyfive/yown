d4f3094b9b432c2557ecd177f84aac49a76b3316
Benoit Sagols <benoit.sagols@gmail.com>
2020-03-29 13:41:59 +0700

    Add Prettier

--- index.js
+++ index.js
@@ -9,69 +9,70 @@ const request = require("request-promise-native");
 const info = require("./package.json");
 
 const commands = {
-	async add(handle, cmd) {
-		const url = `https://github.com/${handle}/archive/${cmd.tag ||
-			cmd.branch}.zip`;
+  async add(handle, cmd) {
+    const url = `https://github.com/${handle}/archive/${
+      cmd.tag || cmd.branch
+    }.zip`;
 
-		// DOWNLOAD
-		console.log(`\nDownloading...`.blue);
-		console.log(url);
+    // DOWNLOAD
+    console.log(`\nDownloading...`.blue);
+    console.log(url);
 
-		let body;
+    let body;
 
-		try {
-			body = await request.get({ url, encoding: null });
-		} catch (err) {
-			console.error(`\nError downloading file`.red);
-			process.exit(1);
-		}
+    try {
+      body = await request.get({ url, encoding: null });
+    } catch (err) {
+      console.error(`\nError downloading file`.red);
+      process.exit(1);
+    }
 
-		console.log(`\nUnzipping...`.blue);
-		const archive = await zip.loadAsync(body);
+    console.log(`\nUnzipping...`.blue);
+    const archive = await zip.loadAsync(body);
 
-		const writing = archive.file(/src/).map(async file => {
-			const filepath = file.name.split("/src/").pop();
-			const dest = `${cmd.dest}/${filepath}`;
+    const writing = archive.file(/src/).map(async file => {
+      const filepath = file.name.split("/src/").pop();
+      const dest = `${cmd.dest}/${filepath}`;
 
-			const exists = await fs.pathExists(dest);
+      const exists = await fs.pathExists(dest);
 
-			if (exists) {
-				console.log(`Skip ${dest}`.grey);
-				return Promise.resolve();
-			}
+      if (exists) {
+        console.log(`Skip ${dest}`.grey);
+        return Promise.resolve();
+      }
 
-			console.log("Copy ".green + dest.white);
+      console.log("Copy ".green + dest.white);
 
-			if (!cmd.force) {
-				return Promise.resolve();
-			}
+      if (!cmd.force) {
+        return Promise.resolve();
+      }
 
-			const buff = await file.async("nodebuffer");
+      const buff = await file.async("nodebuffer");
 
-			return fs.outputFile(dest, buff);
-		});
+      return fs.outputFile(dest, buff);
+    });
 
-		await Promise.all(writing);
+    await Promise.all(writing);
 
-		if (!cmd.force) {
-			console.log("\nDRY RUN".green);
-			console.log("Use --force option to import files");
-		}
-	}
+    if (!cmd.force) {
+      console.log("\nDRY RUN".green);
+      console.log("Use --force option to import files");
+    }
+  }
 };
 
 commander
-	.version(info.version)
-	.command("add <handle>")
-	.option("-d, --dest [folder]", "Specify destination folder", "src")
-	.option("-b, --branch [name]", "Specify git branch to download", "master")
-	.option("-t, --tag", "Specify git tag to download")
-	.option("-f, --force", "No dry run")
-	.action(commands.add);
+  .version(info.version)
+  .command("add <handle>")
+  .option("-d, --dest [folder]", "Specify destination folder", "src")
+  .option("-b, --branch [name]", "Specify git branch to download", "master")
+  .option("-t, --tag", "Specify git tag to download")
+  .option("-f, --force", "No dry run")
+  .action(commands.add);
 
 if (!process.argv.slice(2).length) {
-	commander.outputHelp();
-	process.exit(1);
+  commander.outputHelp();
+  process.exit(1);
 }
 
 commander.parse(process.argv);
