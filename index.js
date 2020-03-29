bb7acea09d35f6ded4a712f146bdcd935e852323
Benoit Sagols <benoit.sagols@gmail.com>
2020-03-29 16:51:17 +0700

    Use Gists API

--- index.js
+++ index.js
@@ -7,67 +7,134 @@ const zip = require("jszip");
 const path = require("path");
 const request = require("request-promise-native");
 const info = require("./package.json");
+const { prompt } = require("enquirer");
+
+// const url = `${apiUrl}/${id}/archive/${
+//       cmd.tag || cmd.branch
+//     }.zip`;
+
+const log = console.log;
+const logError = console.error;
+
+function getFilepath(file) {
+  const filename = file.name.split("/").pop();
+
+  return filename.replace(/\\/g, "/");
+}
 
 const commands = {
-  async add(handle, cmd) {
-    const url = `https://github.com/${handle}/archive/${
-      cmd.tag || cmd.branch
-    }.zip`;
+  async add(id, cmd) {
+    let url;
 
-    // DOWNLOAD
-    console.log(`\nDownloading...`.blue);
-    console.log(url);
+    // Get gist
+    url = `https://api.github.com/gists/${id}`;
 
-    let body;
+    log(`\nFetching gist...`.blue);
+    log(url);
+
+    let data;
 
     try {
-      body = await request.get({ url, encoding: null });
+      data = await request.get({
+        url,
+        headers: { "User-Agent": "eightyfive" },
+        json: true
+      });
     } catch (err) {
-      console.error(`\nError downloading file`.red);
+      logError(`\nError fetching gist`.red);
+      log(err);
       process.exit(1);
     }
 
-    console.log(`\nUnzipping...`.blue);
-    const archive = await zip.loadAsync(body);
+    // Download gist zip
+    url = `https://gist.github.com/${data.owner.login}/${id}/archive/${data.history[0].version}.zip`;
 
-    const writing = archive.file(/src/).map(async file => {
-      const filepath = file.name.split("/src/").pop();
-      const dest = `${cmd.dest}/${filepath}`;
+    log(`\nDownloading gist archive...`.blue);
+    log(url);
 
-      const exists = await fs.pathExists(dest);
+    try {
+      data = await request.get({ url, encoding: null });
+    } catch (err) {
+      logError(`\nError downloading gist archive`.red);
+      log(err);
+      process.exit(1);
+    }
 
-      if (exists) {
-        console.log(`Skip ${dest}`.grey);
-        return Promise.resolve();
+    log(`\nUnzipping gist archive...\n`.blue);
+
+    const archive = await zip.loadAsync(data);
+    const files = Object.values(archive.files);
+    const logs = [];
+
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const filepath = getFilepath(file);
+
+      // Filter
+      if (file.dir || filepath === "yown.json") {
+        continue;
       }
 
-      console.log("Copy ".green + dest.white);
+      // Exists ?
+      const exists = await fs.pathExists(filepath);
 
       if (!cmd.force) {
-        return Promise.resolve();
+        if (exists) {
+          logs.push(filepath.grey + " skip".white);
+        } else {
+          logs.push(filepath.grey + " copy".green);
+        }
+
+        continue;
       }
 
+      // Overwrite ?
+      let overwrite = !exists || cmd.overwrite;
+
+      if (!overwrite) {
+        const { confirmed } = await prompt({
+          type: "confirm",
+          name: "confirmed",
+          message: `Replace ${filepath.grey} ?`
+        });
+
+        overwrite = confirmed;
+      }
+
+      if (!overwrite) {
+        logs.push(filepath.grey + " skipped".white);
+
+        continue;
+      }
+
+      // Copy
       const buff = await file.async("nodebuffer");
 
-      return fs.outputFile(dest, buff);
-    });
+      await fs.outputFile(filepath, buff);
 
-    await Promise.all(writing);
-
-    if (!cmd.force) {
-      console.log("\nDRY RUN".green);
-      console.log("Use --force option to import files");
+      if (exists) {
+        logs.push(filepath.grey + " replaced".red);
+      } else {
+        logs.push(filepath.grey + " copied".green);
+      }
     }
+
+    if (cmd.force) {
+      log(" ");
+    } else {
+      log("\nDRY RUN".green);
+      log("Use --force to copy files\n");
+    }
+
+    logs.forEach(txt => log(txt));
   }
 };
 
 commander
   .version(info.version)
-  .command("add <handle>")
-  .option("-d, --dest [folder]", "Specify destination folder", "src")
-  .option("-b, --branch [name]", "Specify git branch to download", "master")
-  .option("-t, --tag", "Specify git tag to download")
-  .option("-f, --force", "No dry run")
+  .command("add <id>")
+  .option("--force", "No dry run")
+  .option("--overwrite", "Force file overwrite")
   .action(commands.add);
 
 if (!process.argv.slice(2).length) {
