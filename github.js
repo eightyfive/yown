15262d7e555e5e747e68c737f7ed757c88f0b85c
Benoit Sagols <benoit.sagols@gmail.com>
2020-04-05 13:48:31 +0700

    Get all gists

--- github.js
+++ github.js
@@ -4,20 +4,22 @@ const API_URL = "https://api.github.com";
 const GIST_URL = "https://gist.github.com";
 
 module.exports = {
-  getGist(id) {
-    const url = `${API_URL}/gists/${id}`;
+  getGists(ids) {
+    const archives = ids.map((id) =>
+      request
+        .get({
+          url: `${API_URL}/gists/${id}`,
+          headers: { "User-Agent": "yown" },
+          json: true,
+        })
+        .then(({ id, history, owner }) =>
+          request.get({
+            url: `${GIST_URL}/${owner.login}/${id}/archive/${history[0].version}.zip`,
+            encoding: null,
+          })
+        )
+    );
 
-    return request.get({
-      url,
-      headers: { "User-Agent": "yown" },
-      json: true,
-    });
-  },
-
-  getGistArchive({ id, history, owner }) {
-    const url = `${GIST_URL}/${owner.login}/${id}/archive/${history[0].version}.zip`;
-
-    return request.get({ url, encoding: null });
+    return Promise.all(archives);
   },
 };
-
