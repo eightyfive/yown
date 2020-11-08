53ab13fa240b17c4be707e7427daae7eef0128ef
eightyfive <benoit.sagols@gmail.com>
2020-11-08 17:44:44 +0100

    Send computer ID to API

--- src/api.js
+++ src/api.js
@@ -1,15 +1,24 @@
 const request = require('request-promise-native');
+const Computer = require('node-machine-id');
 const Log = require('./logger');
 
 const API_URL = 'https://api.yown.val';
+const computerId = Computer.machineIdSync();
+
+const options = {
+  json: true,
+  rejectUnauthorized: false,
+  headers: {
+    'X-UID': computerId,
+  },
+};
 
 module.exports = {
   find(id) {
     return request
       .get({
+        ...options,
         url: `${API_URL}/${id}`,
-        json: true,
-        rejectUnauthorized: false,
       })
       .catch((err) => Log.die(`Boilerplate '${id}' not found`, err));
   },
