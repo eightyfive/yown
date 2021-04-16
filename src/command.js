932d98dfcf0b980f21effb9f967448d096ad3689
Benoit SAGOLS <benoit.sagols@gmail.com>
2021-04-16 11:35:44 +0800

    Flatten rx stream

--- src/command.js
+++ src/command.js
@@ -1,12 +1,5 @@
-const { from, merge, of } = require('rxjs');
-const {
-  catchError,
-  concatMap,
-  filter,
-  map,
-  mergeMap,
-  tap,
-} = require('rxjs/operators');
+const { from, of } = require('rxjs');
+const { concatMap, map, mergeMap, tap } = require('rxjs/operators');
 const path = require('path');
 const git = require('isomorphic-git');
 const fs = require('fs-extra');
@@ -20,40 +13,31 @@ const Utils = require('./utils');
 
 const YOWNFILE = 'yown.json';
 
-const rePlaceholder = /<(\w+)>/g;
-const placeholders = {};
+const reHolder = /<(\w+)>/g;
+const prompts = {};
 
 module.exports = async function command(args, options) {
   const staled = await getStaleFilenames();
 
-  // Normalize Gist IDs
-  const ids$ = of(...args).pipe(mergeMap((arg) => ofName(arg)));
+  const tasks$ = from(args).pipe(
+    // Normalize gist IDs
+    mergeMap((arg) => ofGistId(arg)),
 
-  // Fetch Gists
-  const gists$ = ids$
-    .pipe(
-      tap((id) => Log.gist(id, 'Fetching Gist')),
-      mergeMap(fetchGist),
-    )
-    .pipe(
-      // Fetch bundled Gist IDs
-      mergeMap((gist) => mergeBundled(gist)),
-      catchError((err) => console.log(err)),
-    )
-    .pipe(
-      // Map real file paths
-      map((gist) => mapFilepaths(gist, gist._config, options)),
-      catchError((err) => console.error(err)),
-    );
+    // Fetch gist
+    mergeMap((id) => ofGist(id)),
 
-  const tasks$ = gists$
-    .pipe(mergeMap((gist) => fromFiles(gist)))
-    // Ignore yown file
-    .pipe(filter((file) => file.filename !== YOWNFILE))
-    // Prompt placeholder in file names
-    .pipe(concatMap((file) => mapPlaceholder(file)))
+    // Parse gist config
+    map((gist) => withConfig(gist)),
+
+    // Map gist files
+    mergeMap(([gist, config]) => fromFiles(gist, config, options)),
+
+    // Prompt replace placeholder in file names
+    concatMap((file) => promptPlaceholder(file)),
+
     // Copy, patch or ignore (task)
-    .pipe(mergeMap((file) => ofTask(file, staled.includes(file._filepath))));
+    mergeMap((file) => ofTask(file, staled.includes(file._filepath))),
+  );
 
   tasks$.subscribe(
     (value) => {},
@@ -66,132 +50,95 @@ module.exports = async function command(args, options) {
   );
 };
 
-function findConfig(files) {
-  const yownFile = files[YOWNFILE];
-
-  if (yownFile) {
-    return JSON.parse(yownFile.content);
-  }
-
-  return {};
-}
-
-function fetchGist(id) {
-  return from(Github.getGist(id)).pipe(
-    // Map config
-    map((gist) => {
-      try {
-        gist._config = findConfig(gist.files);
-      } catch (err) {
-        Log.die('Error parsing yown.json', id);
-      }
-
-      return gist;
-    }),
-  );
-}
-
-async function getStaleFilenames() {
-  const workingDir = process.cwd();
-
-  // Git repo ?
-  const gitPath = path.resolve(workingDir, './.git');
-  const gitExists = await File.exists(gitPath);
-
-  if (!gitExists) {
-    Log.warn('YOLO mode', '(No git repository detected)');
-
-    return [];
-  }
-
-  const files = await git.statusMatrix({ dir: workingDir, fs });
-
-  // https://isomorphic-git.org/docs/en/statusMatrix
-  return files
-    .filter((file) => file[2] !== file[3])
-    .map((file) => `./${file[0]}`);
-}
-
-function mapFilepaths(gist, config, options) {
-  gist._files = Object.values(gist.files).map((file) => {
-    file._filepath = Utils.getFilepath(
-      options.dir || config.dir,
-      file.filename,
-    );
-
-    return file;
-  });
-
-  return gist;
-}
-
-function mapPlaceholder(file) {
-  const [, varName] = rePlaceholder.exec(file._filepath) || [];
-
-  if (!varName) {
-    return from(Promise.resolve(file));
-  }
-
-  let placeholder;
-
-  if (placeholders[varName]) {
-    placeholder = Promise.resolve(placeholders[varName]);
-  } else {
-    Log.info('\n' + file._filepath);
-    placeholder = inquirer
-      .prompt({
-        type: 'input',
-        name: varName,
-        message: `Replace <${varName}> by:`,
-      })
-      .then((answer) => answer[varName]);
-  }
-
-  // Replace placeholder
-  const re = new RegExp(`<${varName}>`, 'g');
-
-  const replaced = placeholder.then((value) => {
-    file._filepath = file._filepath.replace(re, value);
-    file.content = file.content.replace(re, value);
-
-    // Cache
-    placeholders[varName] = value;
-
-    return file;
-  });
-
-  return from(replaced);
-}
-
-function mergeBundled(gist) {
-  const { bundled = [] } = gist._config;
-
-  let bundled$;
-
-  if (bundled.length) {
-    bundled$ = of(...bundled).pipe(
-      tap((id) => Log.gist(id, 'Fetching Gist')),
-      mergeMap(fetchGist),
-    );
-  } else {
-    bundled$ = from([]);
-  }
-
-  return merge(of(gist), bundled$);
-}
-
-function ofName(arg) {
+function ofGistId(arg) {
   const isName = Utils.isName(arg);
 
   if (isName) {
+    // Fetch gist ID
     return from(Api.find(arg)).pipe(tap((id) => Log.gist(id, arg)));
   }
 
   return of(arg);
 }
 
-function fromFiles(gist) {
-  return from(Object.values(gist._files));
+function ofGist(id) {
+  Log.gist(id, 'Fetching Gist');
+
+  return from(Github.getGist(id));
+}
+
+function withConfig(gist) {
+  let config = {};
+
+  const yownFile = gist.files[YOWNFILE];
+
+  if (yownFile) {
+    try {
+      config = JSON.parse(yownFile.content);
+    } catch (err) {
+      Log.die('Error parsing yown.json', gist.id);
+    }
+  }
+
+  return [gist, config];
+}
+
+function fromFiles(gist, config, options) {
+  const files = Object.values(gist.files)
+    .map((file) => {
+      file._filepath = Utils.getFilepath(
+        options.dir || config.dir,
+        file.filename,
+      );
+
+      return file;
+    })
+    .filter((file) => file.filename !== YOWNFILE);
+
+  return from(files);
+}
+
+function promptPlaceholder(file) {
+  const [, holder] = reHolder.exec(file._filepath) || [];
+
+  if (!holder) {
+    return from(Promise.resolve(file));
+  }
+
+  let input;
+
+  if (prompts[holder]) {
+    input = Promise.resolve(prompts[holder]);
+  } else {
+    Log.info('\n' + file._filepath);
+
+    input = inquirer
+      .prompt({
+        type: 'input',
+        name: holder,
+        message: `Replace <${holder}> by:`,
+      })
+      .then((answer) => {
+        const value = answer[holder];
+
+        // Cache
+        prompts[holder] = value;
+
+        return value;
+      });
+  }
+
+  // Replace placeholder
+  const re = new RegExp(`<${holder}>`, 'g');
+
+  const replaced = input.then((value) => {
+    file._filepath = file._filepath.replace(re, value);
+    file.content = file.content.replace(re, value);
+
+    return file;
+  });
+
+  return from(replaced);
 }
 
 function ofTask(file, ignore) {
@@ -215,4 +162,25 @@ function ofTask(file, ignore) {
   return from(File.copy(file.content, filePath)).pipe(
     tap(() => Log.copy(filePath)),
   );
+}
+
+async function getStaleFilenames() {
+  const workingDir = process.cwd();
+
+  // Git repo ?
+  const gitPath = path.resolve(workingDir, './.git');
+  const gitExists = await File.exists(gitPath);
+
+  if (!gitExists) {
+    Log.warn('YOLO mode', '(No git repository detected)');
+
+    return [];
+  }
+
+  const files = await git.statusMatrix({ dir: workingDir, fs });
+
+  // https://isomorphic-git.org/docs/en/statusMatrix
+  return files
+    .filter((file) => file[2] !== file[3])
+    .map((file) => `./${file[0]}`);
 }
