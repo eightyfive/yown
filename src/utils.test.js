2938d157c88fa918881995c693125326d53adaa7
0x55 <0xfiftyfive@gmail.com>
2022-08-31 21:55:25 +0700

    Add utils tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -2,17 +2,31 @@ const Utils = require('./utils');
 
 const fp = Utils.getFilePath;
 
-describe('Filepath', () => {
-  test('Simple', () => {
+describe('Utils', () => {
+  test('Simple filepath', () => {
     expect(fp('foo\\bar.js', '')).toEqual('./foo/bar.js');
   });
 
-  test('Dir', () => {
+  test('Dir filepath', () => {
     expect(fp('foo\\bar.js', '/')).toEqual('./foo/bar.js');
     expect(fp('foo\\bar.js', './')).toEqual('./foo/bar.js');
 
     expect(fp('foo\\bar.js', 'deep')).toEqual('./deep/foo/bar.js');
     expect(fp('foo\\bar.js', 'deep/')).toEqual('./deep/foo/bar.js');
     expect(fp('foo\\bar.js', './deep/')).toEqual('./deep/foo/bar.js');
+  });
+
+  test('isName', () => {
+    expect(Utils.isName('@rnna/navigator')).toEqual(true);
+    expect(Utils.isName('@rnna')).toEqual(false);
+    expect(Utils.isName('rnna')).toEqual(false);
+  });
+
+  test('isAppend', () => {
+    expect(Utils.isAppend('>>index.js')).toEqual(true);
+  });
+
+  test('isPrepend', () => {
+    expect(Utils.isPrepend('<<index.js')).toEqual(true);
   });
 });
